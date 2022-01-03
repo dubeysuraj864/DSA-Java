ac10841e755063797a95659db89ae82439f22bf8
Suraj Dubey <dubeysuraj864@gmail.com>
2022-01-03 15:26:31 +0530

    shtivdf

---
+++ Basics/continue/exp3.java
@@ -0,0 +1,16 @@
+public class exp3 {
+    public static void main(String[] args) {
+        int i=1;
+
+        do{
+            if(i==5){
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+    
+}
