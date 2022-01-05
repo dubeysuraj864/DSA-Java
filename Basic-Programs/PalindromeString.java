d2a113108629b6d26edcbdc7c19c691396c5cb2c
Suraj Dubey <dubeysuraj864@gmail.com>
2022-01-05 15:59:25 +0530

    werfwf

--- Basic-Programs/PalindromeString.java
+++ Basic-Programs/PalindromeString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Palindrome2 {
+public class PalindromeString {
     public static void main(String[] args) {
         String original, reverse = "";
         Scanner scn =  new Scanner(System.in);
@@ -8,15 +8,16 @@ public class Palindrome2 {
         original = scn.nextLine();
 
         int length = original.length();
-        for(int i= length-1 ; i>=0 ;i--){
+        for(int i= length-1 ; i>=0 ;i--)
             reverse = reverse + original.charAt(i);
-            if(original.equals(reverse)){
-                System.out.println("Palindome");
-            }
-            else{
-                System.out.println("Not palindrome");
-            }
+           
 
-        }
+        
+        if(original.equals(reverse))
+            System.out.println("Palindome");
+        
+        else
+            System.out.println("Not palindrome");
+        
     }
 }
---
+++ Basic-Programs/ex.java
@@ -0,0 +1,9 @@
+public class ex {
+    
+    public static void main(String[] args) {
+        String name = "suraj";
+        int length = name.length();
+
+        System.out.println(length);
+    }
+}
