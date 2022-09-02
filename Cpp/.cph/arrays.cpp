7d3c636646bb625850b56f2071b2346d6037a179
Suraj Dubey <dubeysuraj864@gmail.com>
2022-09-02 20:50:37 +0530

    java-basics

---
+++ Cpp/.cph/arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string car[] = {"Volvo", "BMW", "Ford", "Nano", "Tesla","Chevy"};
+    
+    // cout << car[2];
+
+    cout << car->length;
+
+    // for (int i=0 ; i<= sizeof(car) ; i++){
+    //     cout << car[i] << endl;
+    // }
+
+    return 0;
+}
---
+++ Java-Basics/HelloWorld.java
@@ -0,0 +1,30 @@
+public class HelloWorld {
+    public static void main(String [] args){
+     
+        int a = 20;
+        int b = 32;
+
+        int sum = a+b;
+
+        System.out.println(sum);
+
+        boolean apple = true;
+        boolean banana = false;
+
+        byte byteVar = 0;
+
+        short shortVar = 1;
+
+        long longVar = 123434;
+
+        float floatVar = 69.6f;
+
+        double doubVar = 344.42223;
+
+        char c = 's';
+        
+        System.out.println(apple + " " + banana + " " + byteVar + " " + shortVar + " " + longVar + " " + floatVar +" "+ doubVar + " " + c);
+
+
+    }
+}
