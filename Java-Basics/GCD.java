31f2a32bbf0997c6a901500acbc397ca920d91fa
Suraj Dubey <dubeysuraj864@gmail.com>
2022-09-04 12:09:23 +0530

    GCD

---
+++ Java-Basics/GCD.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.math.*;
+
+public class GCD {
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+
+        int gcd = 0;
+
+        for(int i=1 ; i<= a && i<= b; i++){
+         if(a%i==0 && b%i==0 ){
+            gcd = i;
+         
+         }
+        }
+        System.out.println(gcd);
+     
+
+    }
+
+}
