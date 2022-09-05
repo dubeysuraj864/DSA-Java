5f8402008d12813df342ebe27d2cd8ef35af7269
Suraj Dubey <dubeysuraj864@gmail.com>
2022-09-05 11:24:55 +0530

    all prime number till N

--- Java-Basics/AllPrimes.java
+++ Java-Basics/AllPrimes.java
@@ -1,24 +1,29 @@
 import java.util.*;
 
 public class AllPrimes {
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        int N = scn.nextInt();
-        int primes = 0;
+        int n = scn.nextInt();
+        int i = 2;
 
-        for (int i = 2; i <= N; i++) {
-            if (i % 2 != 0) {
+        while (n >= i) {
 
-                for (int j = 1; j <= i; j++) {
-                    if (i == j) {
-                        primes = i;
-                        System.out.println(primes);
-                    }
+            for (int a = 2; a <= i; a++) {
+                if (i % a == 0 && a != i) {
+                    break;
+                } else if (i % a == 0 && a == i) {
+                    System.out.print(a + " ");
+
                 }
 
             }
+
+            i++;
+
         }
 
+        // }
     }
 }
