ce11b277ac682c6f95c38a4c1c3da13eb745ec17
Suraj Dubey <dubeysuraj864@gmail.com>
2022-09-05 12:36:15 +0530

    cd

---
+++ Java-Basics/AllFibo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AllFibo {
+    public static void main(String[] args) {
+
+        // 0 1 1 2 3
+        // a b fib
+
+        Scanner scn = new Scanner(System.in);
+         
+
+        int n = scn.nextInt();
+        int fib = 0;
+        int a = 0;
+        int b = 1;
+        int i = 1;
+
+        while (n >= i) {
+            System.out.print(a + " ");
+            fib = a + b;
+            a = b;
+            b = fib;
+            
+
+            i++;
+        }
+
+    }
+
+}
