b2041a72f16ef55a98c3b62b9311a995e1ae2193
Suraj Dubey <dubeysuraj864@gmail.com>
2022-09-08 15:23:00 +0530

    inverted full pyramid

---
+++ Java-Basics/InvertedFullPyramid.java
@@ -0,0 +1,17 @@
+public class InvertedFullPyramid {
+    public static void main(String[] args) {
+        int n=10;
+
+        for(int i=n; i>=1; i--){
+
+            for(int space =i ; space<=n; space++){
+                System.out.print(" ");
+            } 
+
+            for(int j=i; j>=1;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
