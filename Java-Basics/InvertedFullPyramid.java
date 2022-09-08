0e5c63ac2bca93bfff3f2e840e273583553b8a30
Suraj Dubey <dubeysuraj864@gmail.com>
2022-09-08 15:33:52 +0530

    a

--- Java-Basics/InvertedFullPyramid.java
+++ Java-Basics/InvertedFullPyramid.java
@@ -1,8 +1,20 @@
 public class InvertedFullPyramid {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 6;
 
-        for (int i = n; i >= 1; i--) {
+        for (int i = 1; i <= n; i++) {
+
+            for (int space = i; space <= n; space++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n-1; i >= 1; i--) {
 
             for (int space = i; space <= n; space++) {
                 System.out.print(" ");
